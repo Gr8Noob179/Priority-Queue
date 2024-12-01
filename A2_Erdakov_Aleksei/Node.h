5f14740049076bece6bd35056681a9d96ce20028
Aleksei Erdakov <145776762+Gr8Noob179@users.noreply.github.com>
2024-12-01 07:52:42 -0500

    Node for LinkedList Implemented

--- A2_Erdakov_Aleksei/Node.h
+++ A2_Erdakov_Aleksei/Node.h
@@ -1,1 +1,11 @@
 #pragma once
+
+template <typename T>
+class Node {
+public:
+    int priority;
+    T data;
+    Node* next;
+
+    Node(int priority, T data) : priority(priority), data(data), next(nullptr) {}
+};
